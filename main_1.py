cd7bf72da8c88d217210f14af93c8f4a2be58bcb
Saketh2611 <ae22b061@smail.iitm.ac.in>
2025-06-21 14:26:28 +0530

    Add files via upload

--- main_1.py
+++ main_1.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
 from PIL import Image
 import torch
 import torchvision.transforms as transforms
@@ -8,24 +9,32 @@ from model import Discriminator
 
 app = FastAPI()
 
+# Path for HTML templates
+templates = Jinja2Templates(directory="templates")
+
 # Constants
 image_size = 64
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Image transform
+# Preprocessing transform
 transform = transforms.Compose([
     transforms.Resize((image_size, image_size)),
     transforms.ToTensor(),
     transforms.Normalize([0.5], [0.5])
 ])
 
-# Load trained model
+# Load trained discriminator
 discriminator = Discriminator()
 discriminator.load_state_dict(torch.load(r"C:\Users\v.saketh\Downloads\discriminator.pth", map_location=device))
 discriminator.eval()
 discriminator.to(device)
 
-# API endpoint
+# Homepage: Upload interface
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# Verification endpoint
 @app.post("/verify")
 async def verify_signature(file: UploadFile = File(...)):
     image_bytes = await file.read()
